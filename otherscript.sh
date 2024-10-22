c8707aa1d2274886baf205a5bbc3463f93c92fc8
nsravanigithub <nsravani1287@gmail.com>
2024-10-22 13:45:31 +0530

    some-message

--- otherscript.sh
+++ otherscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-COURSE2="DevOps from Other Script2"
+COURSE="DevOps from Other Script2"
 
-echo "After calling other script, course: $COURSE2"
+echo "After calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
