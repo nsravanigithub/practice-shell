d67f2592dc09e8a5edf0e681da5c44b2f038eb6f
nsravanigithub <nsravani1287@gmail.com>
2024-10-22 14:06:34 +0530

    some-message

--- mysql.sh
+++ mysql.sh
@@ -9,6 +9,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+set -e
+
 userid=$(id -u)
 Validate ()
 {
@@ -30,13 +33,13 @@ exit 1
 fi
 
 dnf install mysql-server -y &>>$LOGFILE
-Validate $? "Installing Mysql server"
+#Validate $? "Installing Mysql server"
 
 systemctl enable mysqld &>>$LOGFILE
-Validate $? "Enabling Mysql server"
+#Validate $? "Enabling Mysql server"
 
 systemctl start mysqld &>>$LOGFILE
-Validate $? "Starting Mysql server"
+#Validate $? "Starting Mysql server"
 
 #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 #Validate $? "Setting Mysql server root password"
@@ -47,7 +50,7 @@ mysql -h db.devops4srav.online -uroot -p${mysql_root_password} -e 'show database
 if [ $? -ne 0 ]
 then
 mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-Validate $? "Setting Mysql server root password"
+#Validate $? "Setting Mysql server root password"
 else
 echo -e "DB root password already set...$Y Skipping $N"
 fi
