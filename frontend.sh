3fc580f325743ab50a899854d1493c5e2bf9b0d1
nsravanigithub <nsravani1287@gmail.com>
2024-10-22 16:52:12 +0530

    some-message

--- frontend.sh
+++ frontend.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
+source ./common.sh
 
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-userid=$(id -u)
-Validate ()
-{
-    if [ $1 -eq 0 ]
-    then
-    echo -e "$2 is:: $G Success $N"
-    else
-    echo -e "$2 is:: $R Failure $N"
-    exit 1
-    fi
-}
-
-if [ $userid -eq 0 ]
-then
-echo "User have root previlages"
-else
-echo "Run with root access"
-exit 1
-fi
+check_root()
 
 dnf install nginx -y &>>$LOGFILE
 Validate $? "Installing nginx"
